cc8975b26d48212483e508170f6455aed926430b
lngLieber6 <galangjihad88@gmail.com>
2025-08-15 11:58:31 +0700

    Fix

--- public/js/user/user-create.js
+++ public/js/user/user-create.js
@@ -16,22 +16,63 @@ async function createUser() {
         return;
     }
 
-  
-    const mutation = `
+    const userMutation = `
         mutation {
             createUser(input: { name: "${name}", email: "${email}", password: "${password}" }) {
                 id
                 name
                 email
-                password
             }
         }
     `;
-    await fetch('/graphql', {
+    const userResponse = await fetch('/graphql', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ query: mutation })
+        body: JSON.stringify({ query: userMutation })
     });
+    const userResult = await userResponse.json();
+
+    if (userResult.errors) {
+        alert('Gagal membuat user: ' + userResult.errors[0].message);
+        return;
+    }
+
+    const newUser = userResult.data.createUser;
+    console.log('User created:', newUser);
+
+    
+    const profileMutation = `
+        mutation {
+            createUserProfile(input: {
+                user_id: ${newUser.id},
+                nama_lengkap: "${newUser.name}",
+                nrp: null,
+                alamat: null,
+                foto: null,
+                bagian_id: null,
+                level_id: null,
+                status_id: null
+            }) {
+                id
+                user_id
+                nama_lengkap
+            }
+        }
+    `;
+    const profileResponse = await fetch('/graphql', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ query: profileMutation })
+    });
+    const profileResult = await profileResponse.json();
+
+    if (profileResult.errors) {
+        alert('Gagal membuat user profile: ' + profileResult.errors[0].message);
+        return;
+    }
+
+    console.log('UserProfile created:', profileResult.data.createUserProfile);
+
     closeAddUserModal();
     loadUserData();
 }
